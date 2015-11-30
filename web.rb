b3873d9633c00d8b837281a3c087411e24c325da
Daniel Naranjo <dnaranjo@gmail.com>
2015-11-29 23:35:53 -0300

    Changes

--- web.rb
+++ web.rb
@@ -107,8 +107,8 @@ get '/tweetbyuser' do
 		config.access_token = "110495478-qnrKkkokaooS4xZhfjwI3m2xL9Mj5gF6xKFW5Lsh"
 		config.access_token_secret = "IRyN7oP4lPMQzv7Glhqc5J1dDM6p578gyJ3XBjalX17fG"
 	end
-	session[:text] = client.user(username)
+	text = client.user('naranjodaniel')
 	<<-HTML
-		#{:text}
+		text
 	HTML
 end
