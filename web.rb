c2e711fb4b47d23bd51d2c5004235bbf6b0dbeb4
Daniel Naranjo <dnaranjo@gmail.com>
2015-11-29 22:43:08 -0300

    Changes

--- web.rb
+++ web.rb
@@ -83,7 +83,7 @@ get '/logout' do
 	HTML
 end
 
-get 'tweet' do
+get '/tweet' do
 	client = Twitter::REST::Client.new do |config|
 		config.consumer_key = "kVdTORs1LCUtcJXDE5AXm1WW9"
 		config.consumer_secret = "pPZ6uJPEyT1jWyi0N00yNa1c18w79zDBqht3rL2GvvkIR3vYBf"
